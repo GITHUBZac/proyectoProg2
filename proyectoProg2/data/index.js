790752ac155936c49d46f165808e58b4a9fd1acb
GITHUBZac <zofarrell@udesa.edu.ar>
2022-09-15 08:51:44 -0300

    fotos usuarios

--- proyectoProg2/data/index.js
+++ proyectoProg2/data/index.js
@@ -2,7 +2,7 @@ const data = {
     usuarios: [
         {
             nombre: 'Lionel Messi',
-            foto: '"https://www.google.com/imgres?imgurl=https%3A%2F%2Festaticos-cdn.elperiodico.com%2Fclip%2F69964ce0-68a6-4758-bef5-6503eff109e0_alta-libre-aspect-ratio_default_0.jpg&imgrefurl=https%3A%2F%2Fwww.elperiodico.com%2Fes%2Fdeportes%2F20190306%2Fmessi-vuelve-seleccion-argentina-7275815&tbnid=oVsQ0J1fz6jifM&vet=12ahUKEwjVzfDFzpX6AhUOnpUCHV5uB1sQMygKegUIARDrAQ..i&docid=ucJxsgSfy3MGnM&w=1200&h=674&q=lionel%20messi%20seleccion%20argentina&ved=2ahUKEwjVzfDFzpX6AhUOnpUCHV5uB1sQMygKegUIARDrAQ/250x250"',
+            foto: "https://estaticos-cdn.elperiodico.com/clip/69964ce0-68a6-4758-bef5-6503eff109e0_alta-libre-aspect-ratio_default_0.jpg" ,
             mail: 'liomessi@gmail.com',
             contrasenia: 'MESSI',
             fecha: '1987-6-24',
@@ -14,7 +14,7 @@ const data = {
 
         {
             nombre: 'Lautaro Martinez',
-            foto: '"https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.a24.com%2Fp%2F00fbb826805afe81d4a9a7567d553335%2Fadjuntos%2F296%2Fimagenes%2F008%2F760%2F0008760331%2F1200x675%2Fsmart%2Flautaro_1200jpg.jpg&imgrefurl=https%3A%2F%2Fwww.a24.com%2Fovacion%2Flos-brillantes-numeros-lautaro-martinez-la-seleccion-n994159&tbnid=lj53gFf4dyDAZM&vet=12ahUKEwjniteiz5X6AhUitpUCHe67DwgQMygDegUIARDJAQ..i&docid=UwPKd8TK1aqJFM&w=1200&h=675&q=lautaro%20martinez%20seleccion&ved=2ahUKEwjniteiz5X6AhUitpUCHe67DwgQMygDegUIARDJAQ/250x250"',
+            foto: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSc6OGPEfKYW-YSjVsc0SUuFAZ1FUgDlT2ywg&usqp=CAU",
             mail: 'lautamartinez@gmail.com',
             contrasenia: 'toro',
             fecha: '1997-8-22',
@@ -26,7 +26,7 @@ const data = {
 
         {
             nombre: 'Angel Di Maria',
-            foto: '"https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.elgrafico.com.ar%2Fmedia%2Fcache%2Fpub_news_details_large%2Fmedia%2Fi%2F64%2F82%2F6482fd42a690af0869ce3b00c4d638be82ca4995.jpeg&imgrefurl=https%3A%2F%2Fwww.elgrafico.com.ar%2Farticulo%2F1116%2F45288%2Flas-confesiones-de-angel-di-maria&tbnid=LWsUpLMx5CRv2M&vet=12ahUKEwiBndLpz5X6AhVuspUCHXmODjkQMygCegUIARC6AQ..i&docid=g7bJ3acKe4yX7M&w=1463&h=900&q=angel%20di%20maria%20seleccion%20argentina&ved=2ahUKEwiBndLpz5X6AhVuspUCHXmODjkQMygCegUIARC6AQ/250x250"',
+            foto: "https://s3.amazonaws.com/arc-wordpress-client-uploads/infobae-wp/wp-content/uploads/2018/06/30115405/Argentina-vs-Francia-Mundial-Rusia-2018-86.jpg",
             mail: 'fideodima@gmail.com',
             contrasenia: 'fideo.corazon',
             fecha: '1988-2-14',
@@ -38,7 +38,7 @@ const data = {
 
         {
             nombre: 'Rodrigo De Paul',
-            foto: '"https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ole.com.ar%2F2021%2F11%2F16%2FyKKpjQujC_2000x1500__1.jpg&imgrefurl=https%3A%2F%2Fwww.ole.com.ar%2Fseleccion%2Fpaul-camiseta-seleccion-cansado_0_vUqFkym8Y.html&tbnid=Rbu7Auq3t1CiXM&vet=12ahUKEwil1O2y0JX6AhXElZUCHZCcDvwQMygDegUIARDFAQ..i&docid=NdzOBDhPwswAWM&w=2000&h=1500&q=rodrigo%20de%20paul%20seleccion%20argentina&ved=2ahUKEwil1O2y0JX6AhXElZUCHZCcDvwQMygDegUIARDFAQ/250x250"',
+            foto: "https://images.ole.com.ar/2021/11/16/yKKpjQujC_2000x1500__1.jpg",
             mail: 'rodridepa@gmail.com',
             contrasenia: 'TripleT',
             fecha: '1994-5-24',
@@ -50,7 +50,7 @@ const data = {
 
         {
             nombre: 'Cristian Romero',
-            foto: '"https://www.google.com/imgres?imgurl=https%3A%2F%2Fimages.ole.com.ar%2F2021%2F06%2F04%2FDqzBV61tH_340x340__1.jpg&imgrefurl=https%3A%2F%2Fwww.ole.com.ar%2Fseleccion%2Fcuti-romero-debut-argentina-premier_0_7XXthU5Ee.html&tbnid=2B96m01ZNdnbBM&vet=12ahUKEwjtrvTQ0JX6AhVBvJUCHZ65ALcQMygLegUIARC0AQ..i&docid=YV9ux1ZfHThF_M&w=340&h=340&q=cristian%20romerseleccion%20argentina&ved=2ahUKEwjtrvTQ0JX6AhVBvJUCHZ65ALcQMygLegUIARC0AQ/250x250"',
+            foto: "https://images.ole.com.ar/2021/07/12/3tMEaKvze_340x340__1.jpg",
             mail: 'cutiro@gmail.com',
             contrasenia: 'Cutineta1',
             fecha: '1998-4-27',
