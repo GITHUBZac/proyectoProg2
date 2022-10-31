acb7f4b80c5c8b3e1bb59cd56573735ee992adfd
tomasarizu <tarizu@udesa.edu.ar>
2022-10-31 09:34:03 -0300

    cosas

---
+++ miApp/data/miPerfil.js
@@ -0,0 +1,64 @@
+let data = {
+    listaProductos: [
+        {
+            id: 1,
+            nombre: "Iphone 13",
+            marca: "Apple",
+            precio: 400
+        },
+        {
+            id: 2,
+            nombre: "Iphone 14",
+            marca: "Apple",
+            precio: 1000
+        },
+        {
+            id:3,
+            nombre: "Iphone 12",
+            marca: "Apple",
+            precio: 200
+        },
+        {
+            id: 4,
+            nombre: "Samsung S21",
+            marca: "Samsung",
+            precio: 250
+        },
+        {
+            id: 5,
+            nombre: "Samsung S22",
+            marca: "Samsung",
+            precio: 350
+        },
+        {
+            id: 6,
+            nombre: "Motorola G9",
+            marca: "Motorola",
+            precio: 150
+        },
+        {
+            id: 7,
+            nombre: "Iphone 14",
+            marca: "Apple",
+            precio: 1000
+        },
+        {
+            id: 8,
+            nombre: "Motorola G8 Plus ",
+            marca: "Motorola",
+            precio: 225
+        },
+        {
+            id: 9,
+            nombre: "Motorola One",
+            marca: "Motorola",
+            precio: 300
+        },
+        {
+            id: 10,
+            nombre: "Huawei y9",
+            marca: "Huawei",
+            precio: 500
+        },
+    ]
+}
