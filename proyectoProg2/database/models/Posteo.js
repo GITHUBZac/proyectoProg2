9370cde1410ce9185de6bc9235fd9f781993ced0
GITHUBZac <zofarrell@udesa.edu.ar>
2022-11-10 08:17:41 -0300

    modelo posteo

---
+++ proyectoProg2/database/models/Posteo.js
@@ -0,0 +1,48 @@
+module.exports = function (sequelize, dataTypes) {
+    let alias = 'Posteo';
+
+    let cols = {
+        id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        id_usuarios: {
+            type: dataTypes.INTEGER
+        },  
+       image_name:{
+        type: dataTypes.STRING
+       },
+       pie_post:{
+        type: dataTypes.STRING
+       },
+        createdAt: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        },
+        updatedAt: {
+            type: dataTypes.DATE,
+            allowNull: true,
+        }
+    }
+
+    let config ={
+        tableName: "posteo",
+        timestamps: false,
+        underscored: false,
+    }
+
+    let Posteo = sequelize.define(alias,cols, config);
+    
+    Posteo.associate = function(models) {
+        Posteo.hasMany( models.Comentario, {
+            as : 'comentario',
+            foreignKey : 'id_posteos',
+        })
+        Posteo.belongsTo(models.Usuario, {
+            as:'usuario',
+            foreignKey: 'id_usuarios',
+        })
+    }
+    return Posteo ;
+}
