0432c554c0bd2b3d1c1463d5ddc19e6ec59c3390
GITHUBZac <zofarrell@udesa.edu.ar>
2022-11-19 13:35:13 -0300

    eliminar

--- proyectoProg2/routes/post.js
+++ proyectoProg2/routes/post.js
@@ -4,11 +4,12 @@ var postController = require('../controllers/postController');
 
 let multer = require('multer');
 let path = require('path');
+const { Router } = require('express');
 
 /* Configurar multer */
 let storage = multer.diskStorage({
     destination: function(req, file, cb) {
-        cb(null, path.join(__dirname, '../public/img/posts'));
+        cb(null, path.join(__dirname, '../public/img/post'));
     },
     filename: function(req, file, cb) {
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
@@ -21,7 +22,13 @@ let upload = multer({storage: storage});
 router.get('/nuevoPost', postController.nuevoPost);
 router.post('/nuevoPost', upload.single('imagen'), postController.crearPost);
 
-router.post('/detallePost', postController.detallePost);
+/* aqui guarda un posteo */
+router.post('/guardarPost:', postController.detallePost);
+
+/* Buscar detalle del posteos numero :id */
 router.get('/detallePost/:id', postController.detallePost);
 
+router.get("/resultado-busqueda",postController.busquedaPost)
+
+router.get("/eliminarPost/:id",postController.eliminarPost)
 module.exports = router;
