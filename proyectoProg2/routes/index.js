b5bae63905c1c0443c279d265591954a5f7c4578
GITHUBZac <zofarrell@udesa.edu.ar>
2022-11-19 13:01:07 -0300

    asassa

--- proyectoProg2/routes/index.js
+++ proyectoProg2/routes/index.js
@@ -4,6 +4,5 @@ var indexController = require('../controllers/indexController')
 
 /* GET home page. */
 router.get('/', indexController.index );
-router.get('/busqueda', indexController.index);
 
 module.exports = router;
